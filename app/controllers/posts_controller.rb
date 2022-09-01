ed9481daf623597888968754e6929a38924bcffd
Olivia <oliviatmanalastas@gmail.com>
2022-09-01 17:09:30 -0400

    Done with assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,19 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+		# added rescue_from
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+		render json: {errors: invalid.record.errors}, status: :unprocessable_entity
   end
 
   private
